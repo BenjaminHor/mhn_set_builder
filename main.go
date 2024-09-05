27d8849472b25bff1bdd72dd633e9dec36087f68
Ben Hor <bhor@stavvy.com>
2024-09-05 12:12:19 -0400

    initial function to search and find armor sets based on provided skill list

--- main.go
+++ main.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type ArmorSets struct {
+	ArmorSets []ArmorSet `json:"armor_sets"`
+}
+
+type ArmorSet struct {
+	Name   string  `json:"name"`
+	Type   string  `json:"type"`
+	Skills []Skill `json:"skills"`
+}
+
+type Skill struct {
+	Name  string `json:"name"`
+	Level int    `json:"level"`
+	Grade int    `json:"grade"`
+}
+
+var armor_collection ArmorSets
+
 func main() {
+	readArmorCollection()
+	findArmorSets([]Skill{
+		{
+			Name:  "Focus",
+			Level: 1,
+		},
+		{
+			Name:  "Attack Boost",
+			Level: 1,
+		},
+	})
+}
+
+func readArmorCollection() {
+	bytes, _ := os.ReadFile("assets/armor_collection.json")
+	json.Unmarshal(bytes, &armor_collection)
+}
+
+func findArmorSets(skills []Skill) {
+	for _, skill := range skills {
+		fmt.Println(skill)
+	}
 }
